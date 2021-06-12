4641e58d18ddc813abf32bf59eef098c9dcccbfd
LaTonya Johnson <latonya.johnson0921@gmail.com>
2021-06-11 22:28:57 -0500

    random gifs

--- app.js
+++ app.js
@@ -18,9 +18,12 @@ document.body.append(remove);
 
 
 async function getInfo(term) {
-    const res = await axios.get(`http://api.giphy.com/v1/gifs/search`, {params: {api_key:'eZCmudZGVCC0EhR5wMDs238jMQebVc89', q:`${term}`, limit:1}});
-    console.log(res.data.data[0]);
-    let gif = res.data.data[0].images.original.url;
+    const res = await axios.get(`http://api.giphy.com/v1/gifs/search`, {params: {api_key:'eZCmudZGVCC0EhR5wMDs238jMQebVc89', q:`${term}`}});
+    console.log(res.data.data);
+    let dataLen = res.data.data.length;
+    //console.log(dataLen);
+    let idx = Math.floor(Math.random()*dataLen);
+    let gif = res.data.data[idx].images.original.url;
     const imgContainer = document.createElement('div');
     const img = document.createElement('img');
     img.classList.add('img');
